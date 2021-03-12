bf93b315fde803fe7580e7b3f29f9b7c192a85e9
Arpit Maheshwari <arpitmaheshwari@users.noreply.github.com>
2021-03-12 22:32:58 +0530

    Adding hotjar

--- assets/analytics/google-analytics.js
+++ assets/analytics/google-analytics.js
@@ -5,3 +5,15 @@
 
 ga('create', 'UA-76853532-1', 'auto');
 ga('send', 'pageview');
+
+<!-- Hotjar Tracking Code for www.arpitmaheshwari.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2294599,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
